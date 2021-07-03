6470e5e51dd03e13ea1b963228f206ea56bf4c87
cem <cemsari@outlook.com.tr>
2021-07-04 00:12:06 +0300

    Continue

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const mongoose          = require("mongoose"),
       express           = require("express"),
       bodyParser        = require("body-parser"),
+      passport          = require("passport"),
+      LocalStrategy     = require("passport-local"),
+      User              = require("./models/userModel"),
+      expressSession    = require("express-session"),
       app               = express();
 
 const indexRoutes       = require("./routes/indexRoutes");
 
-
 mongoose.connect("mongodb://localhost/krizmasasi", { useNewUrlParser: true, useUnifiedTopology: true });
 app.set('view engine', 'ejs' );
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use(require("express-session")({
+    secret:"Guvenlik Cumlemiz",
+    resave:false,
+    saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next)=>{
+    res.locals.currentUser=req.user;
+    next();
+});
 
 app.use(indexRoutes);
 
--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,20 +1,70 @@
-const   express       = require("express");
-        router        = express.Router();
+const   express       = require("express"),
+        router        = express.Router(),
+        passport      = require("passport"),
+        User          = require("../models/userModel"),
+        app           = express();
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()) {
+        return next();
+    } else
+    res.redirect("/signin")
+};
+
+function isLoggedOut(req, res, next){
+   if(req.isAuthenticated()) {
+       res.redirect("/")
+   } else
+   return next();
+};
 
 router.get("/", (req, res)=>{
     res.render('welcome');
-});
-
-router.get("/signin", (req, res)=>{
-    res.render('signin');
 });
 
 router.get("/signup", (req, res)=>{
     res.render('signup');
 });
 
+router.post("/signup", (req, res)=>{
+
+    let newUser = new User({username:req.body.username});
+    User.register(newUser, req.body.password, (err, User)=>{
+        if(err){
+            console.log(err);
+            res.redirect("/");
+        }
+        passport.authenticate("local")(req, res, ()=>{
+            res.redirect('/signin');
+        });
+    })
+});
+
+router.get("/signin", (req, res)=>{
+    res.render('signin');
+});
+
+router.post("/signin", passport.authenticate("local",
+    {
+        successRedirect:"/soon", //eslesiyorsa
+        failureRedirect:"/signin" //eslesmiyorsa
+    }),(req, res)=>{});
+
+router.get("/soon", (req, res)=>{
+        res.render('soon');
+});
+
+router.get("/room", (req, res)=>{
+    res.render('room');
+});
+
 router.post("/", (req, res)=>{
     console.log(req.body);
+});
+
+router.get("/signout", (req, res)=>{
+    req.logout();
+    res.redirect("/room");
 });
 
 module.exports = router;
